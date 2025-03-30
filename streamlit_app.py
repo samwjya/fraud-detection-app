403a14694c7cc231af5e3b36645bb91d6f4e4344
samwjya <samuel67@illinois.edu>
2025-03-30 02:44:39 -0500

    Made the dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,22 @@
 import streamlit as st
 import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import io
 
-st.title("💳 Fraud Detection Dashboard")
-st.markdown("Upload a CSV file containing transaction data to check for fraud.")
+st.set_page_config(page_title="Fraud Detection Dashboard", page_icon="💳", layout = "centered")
+
+st.markdown("<h1 style='text-align: center; color: #4CAF50;'>💳 Fraud Detection Dashboard</h1>", unsafe_allow_html=True)
+st.write("Upload a CSV file below to detect whether a transaction is fraudulent or not.")
+st.markdown("---")
+
+with st.sidebar:
+    st.markdown("### 📁 Project Info")
+    st.markdown("- Built with Streamlit + FastAPI")
+    st.markdown("- Random Forest model")
+    st.markdown("---")
+    st.markdown("👤 Created by: Samuel Wijaya")
+
 
 uploaded_file = st.file_uploader("Uplaod a CSV File", type=["csv"])
 
@@ -23,15 +37,58 @@ if uploaded_file is not None:
     st.write("Formatted input for model: ")
     st.json(model_input)
 
-    if st.button("Predict"):
+    if st.button("Analyze All Transactions"):
         try:
-            response = response.post("http://127.0.0.1:8000/predict", json=model_input)
-            result = response.json()
+            fraud_count = 0
+            not_fraud_count = 0
+            total = df.shape[0]
+            predictions = []
+            
+            for index, row in df.iterrows():
+                model_input = {feature: 0.0 for feature in expected_features}
 
-            if result.get("fraud") == True:
-                st.error("🚨 Fraudulent Transaction Detected!")
-            else:
-                st.success("✅ This transaction is safe.")
+                for col in df.columns:
+                    if col in model_input:
+                        model_input[col] = float(row[col])
+               
+                response = requests.post("http://127.0.0.1:8000/predict", json=model_input)
+                result = response.json()
+
+                is_fraud = result.get("fraud", False)
+                predictions.append("Fraud! " if is_fraud else "Safe ")
+
+                if is_fraud:
+                    fraud_count += 1
+                else:
+                    not_fraud_count += 1
+
+            st.success(f"✅ Total Transactions: {total}")
+            st.error(f"🚨 Predicted Fraudulent Transactions: {fraud_count}")
+            st.info(f"✅ Predicted Safe Transactions: {not_fraud_count}")
+
+            st.markdown("### Class Distribution")
+            labels = ["Not Fraud", "Fraud"]
+            values = [not_fraud_count, fraud_count]
+            colors=["#66bb6a", "#ef5350"]
+            fig, ax = plt.subplots()
+
+            ax.pie(values, labels=labels, autopct="%1.1f%%", startangle=90, colors=colors )
+            ax.axis("equal")
+            st.markdown("### 🧮 Prediction Summary")
+            st.pyplot(fig)
+
+            df["predictions"] = predictions
+
+            csv_buffer = io.StringIO()
+            df.to_csv(csv_buffer, index = False)
+            csv_data = csv_buffer.getvalue().encode("utf-8")
+
+            st.download_button(
+                label="Download results as CSV",
+                data=csv_data,
+                file_name="predicted_results.csv",
+                mime="text/csv"
+            )
         
         except Exception as e:
             st.error(f"Error: {str(e)}")
